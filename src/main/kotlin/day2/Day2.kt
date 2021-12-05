7aca3b744789381815a11dc5f708087afa4ee8e8
Joe Slavinski <joeslavinski89@gmail.com>
2021-12-05 13:35:41 -0500

    Modified to include Pairs

--- src/main/kotlin/day2/Day2.kt
+++ src/main/kotlin/day2/Day2.kt
@@ -1,38 +1,39 @@
 package day2
 import java.io.File
 
-fun trackSubMulti(instructions: List<List<String>>) : Int {
+fun trackSubMulti(instructions : List<Pair<String, Int>>) : Int {
     var horizontal = 0
     var depth = 0
-    for (instruction in instructions) {
-        when (instruction[0]) {
-            "forward" -> horizontal += instruction[1].toInt()
-            "up" -> depth -= instruction[1].toInt()
-            "down" -> depth += instruction[1].toInt()
+    for ((command, amt) in instructions) {
+        when (command) {
+            "forward" -> horizontal += amt
+            "up" -> depth -= amt
+            "down" -> depth += amt
         }
     }
     return (horizontal * depth)
 }
 
-fun trackSubAim(instructions: List<List<String>>) : Int {
+fun trackSubAim(instructions : List<Pair<String, Int>>) : Int {
     var horizontal = 0
     var depth = 0
     var aim = 0
-    for (instruction in instructions) {
-        when (instruction[0]) {
+    for ((command, amt) in instructions) {
+        when (command) {
             "forward" -> {
-                horizontal += instruction[1].toInt()
-                depth += (aim * instruction[1].toInt())
+                horizontal += amt
+                depth += (aim * amt)
             }
-            "up" -> aim -= instruction[1].toInt()
-            "down" -> aim += instruction[1].toInt()
+            "up" -> aim -= amt
+            "down" -> aim += amt
         }
     }
     return (horizontal * depth)
 }
 
 fun main() {
-    val instructions = File("src/main/kotlin/Day 2/day2.txt").readLines().map { it.split(" ")}
+    val instructions = File("src/main/kotlin/day2/day2.txt").readLines().map { it.split(" ")
+        .let { (direction, value) -> Pair(direction, value.toInt())}}
     val answer1 = trackSubMulti(instructions)
     println(answer1)
     val answer2 = trackSubAim(instructions)
